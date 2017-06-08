03631aedf22f8b1cf59f43738e9077aa59ae037e
stizastiza <kostjaigin@gmail.com>
2017-06-08 16:11:16 +0200

    Add files via upload

--- FCFS.c
+++ FCFS.c
@@ -7,16 +7,16 @@ void schedule_FCFS(const TaskPool *task_pool) {
     int curr = 0;
     while (!allDone(task_pool)) {
         //Here: select a task for execution, based on his arrival time
-            for (int i = 0; i < task_pool->size; i++) {
-                    if (!isDone(task_pool->task[i])) {
-                        curr = i;
-                        break;
-                    }
+        for (int i = 0; i < task_pool->size; i++) {
+            if (!isDone(checkArrivals(task_pool, i))) {
+                curr = i;
+                if (checkArrivals(task_pool, curr) != NULL) {
+                    break;
+                }
             }
-
+        }
         CPU = checkArrivals(task_pool, curr);
         curr = 0;
-
         if (execTask(CPU, 1) < 0) {
             printf("%sERROR:%s No Task selected to be executed.\n", COLOR_RED, COLOR_RESET);
             break;
